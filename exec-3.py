199fccbc7d22650835dc7be6643aca65ffb277a4
Gui-has <guiesteves30@gmail.com>
2024-08-28 20:53:53 -0300

    Create exec-3.py

---
+++ exec-3.py
@@ -0,0 +1,21 @@
+#Questão 3 - soidfasodfjsadjfsajdfoisajdfds
+
+a=(4/2)+(2/4)
+print(a)
+
+b=4/(2+2)/4
+print(b)
+
+c=(4+2)*2-4
+print(c)
+
+a=4/2+2/4
+print(a)
+
+b=4/2+2/4
+print(b)
+
+c=4+2*2-4
+print(c)
+
+NOTA: OS RESULTADOS NÃO SÃO OS MESMOS
