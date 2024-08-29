ac973c1264c77cbafb269c6c4d5993410ba51d16
Gui-has <guiesteves30@gmail.com>
2024-08-28 21:04:39 -0300

    Create exec-13.py

---
+++ exec-13.py
@@ -0,0 +1,14 @@
+#Questão 13
+
+print("Digite sua primeira nota:")
+nota_1 = float(input())
+
+print("Digite sua segunda nota:")
+nota_2 = float(input())
+
+print("Digite sua terceira nota:")
+nota_3 = float(input())
+
+media_final = ((nota_1 * 2) + (nota_2 * 3) + (nota_3 * 5)) / 10
+
+print("Media final: " + str(media_final))
