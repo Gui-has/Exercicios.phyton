cde054400d0429c65c18df8a049eded964e87cab
Gui-has <guiesteves30@gmail.com>
2024-08-28 21:06:55 -0300

    Create exec-16.py

---
+++ exec-16.py
@@ -0,0 +1,12 @@
+#Questão 16
+
+print("Escreva nota 1")
+nota1 = float(input())
+print("escreva nota 2")
+nota2 = float(input())
+media= nota1+nota2/2
+
+if media >= 6:
+    print("aprovado")
+else:
+    print("reprovado")
