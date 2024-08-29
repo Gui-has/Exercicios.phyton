76b1e28daf626c4ff9bb22cc923756a3b67ce653
Gui-has <guiesteves30@gmail.com>
2024-08-28 21:34:33 -0300

    Create exec-10.py

---
+++ exec-10.py
@@ -0,0 +1,8 @@
+#questão 10
+print("Qual o custo de fabricado carro:)
+custo_fabrica= float(input() 
+ distribuidor=(28/100) * custo de fabrica
+imposto(45/100) * custo de fabrica
+custo_consumidor= custo_fabrica+distirbuidor+imposto
+print("custo final do carro:+str(custo_consumidor))
+      
