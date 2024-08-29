3f423c15a2f92fa4983d0b43e1df700da125ce84
Gui-has <guiesteves30@gmail.com>
2024-08-28 21:02:50 -0300

    Create exec-12.py

---
+++ exec-12.py
@@ -0,0 +1,8 @@
+#Questão 12
+
+print("Digite a temperatura em Fahrenheit: ")
+temp_fah = float(input())
+
+temp_celsius = ((temp_fah - 32) / 9) * 5
+
+print("Temperatura em Celsius: " + str(temp_celsius))
