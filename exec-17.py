357662d3e7cec9424c313ca6666dafb2e84f73e6
Gui-has <guiesteves30@gmail.com>
2024-09-02 18:47:50 -0300

    Update and rename exec-16.py to exec-17.py

--- exec-17.py
+++ exec-17.py
@@ -1,10 +1,11 @@
-#Questão 16
+#Questão 17
 
-print("Escreva nota 1")
-nota1 = float(input())
+print("Escreva nota 1 
+ nota1= float(input())
 print("escreva nota 2")
 nota2 = float(input())
 media= nota1+nota2/2
+
 
 if media >= 6:
     print("aprovado")
