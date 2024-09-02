924c4fbdc391d58e6f1f835056c22c6a136b62ca
Gui-has <guiesteves30@gmail.com>
2024-09-02 19:53:16 -0300

    Update exec-17.py

--- exec-17.py
+++ exec-17.py
@@ -1,4 +1,3 @@
-#Questão 17
 
 print("Escreva nota 1")
  nota1= float(input())
@@ -6,8 +5,8 @@ print("escreva nota 2")
 nota2 = float(input())
 media= nota1+nota2/2
 
-
 if media >= 6:
     print("aprovado")
 else:
     print("reprovado")
+ print(media)
