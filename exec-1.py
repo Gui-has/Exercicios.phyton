c29a6178725ea90b08192dcfc589f89ce2f7f25c
Gui-has <guiesteves30@gmail.com>
2024-09-04 19:44:01 -0300

    Update exec-1.py

--- exec-1.py
+++ exec-1.py
@@ -1,4 +1,4 @@
-#Questão 1
+#escreva um algoritimoque armazene o valor 10 em uma variável A e o valor 20 em uma variável B . a seguir (utilizando atribuições entre variáveis) troque os seus conteudos fazendo com que o valorestá em A passe para B e vice -versa 
 A = 10
 B = 20
 C = 0
